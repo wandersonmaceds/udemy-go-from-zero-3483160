d43937d31c05770c2914612279cb277f80fe30bb
Wanderson M. <wandersonmaceds@gmail.com>
2026-09-16 16:19:55 -0300

    packages: using external dependencies

--- packages-1/go.mod
+++ packages-1/go.mod
@@ -1,3 +1,5 @@
 module module
 
 go 1.25.6
+
+require github.com/badoux/checkmail v1.2.4 // indirect
--- packages-1/main.go
+++ packages-1/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"module/utils"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writting from main!")
 	utils.Write()
+	error := checkmail.ValidateFormat("123")
+	noerror := checkmail.ValidateFormat("user@mail.com")
+
+	fmt.Println(error)
+	fmt.Println(noerror)
 }
