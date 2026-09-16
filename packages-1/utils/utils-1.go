9a65eba690ac23dbacccffce4057a03c1ed92db2
Wanderson M. <wandersonmaceds@gmail.com>
2026-09-16 14:28:17 -0300

    comment: adding tiny comments to remember some utility aspects of go

--- packages-1/utils/utils-1.go
+++ packages-1/utils/utils-1.go
@@ -3,6 +3,7 @@ package utils
 import "fmt"
 
 // the Write functions only writes a dummy messate on the default output
+// uppercased functions are exported by default to be used in other packages of the module
 func Write() {
 	fmt.Println("Writting from Utils 1")
 	write()
--- packages-1/utils/utils-2.go
+++ packages-1/utils/utils-2.go
@@ -2,6 +2,7 @@ package utils
 
 import "fmt"
 
+// lowercased funcions are visible/acessible only in the same package
 func write() {
 	fmt.Println("Write from Utils 2")
 }
